7eae6069d059d817b4de18e3a4ccbdeb59cbb1af
Naval Singh <navalsingh8321@gmail.com>
2026-06-19 02:03:33 +0530

    Time: 8 ms (83.62%) | Memory: 11.7 MB (80.97%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -6,4 +6,16 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution { public: bool hasCycle(ListNode *head) { unordered_map<ListNode* ,int> mpp; ListNode* temp = head; while(temp != nullptr){ if(mpp.find(temp)!=mpp.end()){ return true; } mpp[temp]=1; temp = temp->next; } return false; } };
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+};
